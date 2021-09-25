27c6cad3dc8a530ae780a79458e6dc0774ea2f56
ItsAvi_ <79397844+Avi2427@users.noreply.github.com>
2021-09-25 18:22:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.cooldowns = new Discord.Collection(); //an collection for cooldown comman
   }
 });
 //login into the bot
-client.login("ODI3MDI5NjI1OTc2NTIwNzE0.YGVFXA.sLn6pRUOSTahyv_j9uhxomDQmcU");
+client.login("ODI3MDI5NjI1OTc2NTIwNzE0.YGVFXA.sLn6pRUOSTahyvmcU");
 
 const Enmap = require("enmap")
 client.settings = new Enmap({name: "settings", dataDir: "./database/settings"})
